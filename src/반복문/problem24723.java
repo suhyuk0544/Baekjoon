1d2f202e40c6de2952136bc532f2ceb26a37db86
suhyuk0544 <85779660+suhyuk0544@users.noreply.github.com>
2022-04-30 18:40:57 +0900

    update

--- src/반복문/problem24723.java
+++ src/반복문/problem24723.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class um {
+public class problem24723 {
     public static void main(String[] args) throws IOException {
 
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
