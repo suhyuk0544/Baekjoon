4b9bd8a4362db0d31c2a9f0f99891fe55d2f3a52
suhyuk0544 <85779660+suhyuk0544@users.noreply.github.com>
2022-05-12 19:16:15 +0900

    update

---
+++ src/함수/problem1065.java
@@ -0,0 +1,37 @@
+package 함수;
+
+import java.io.*;
+
+public class problem1065 {
+    static boolean num(int n){
+        if (n < 100){
+            return false;
+        }
+        int[] result = new int[(int) Math.log10(n) +1];
+        for (int i = 0; i < result.length; i++) {
+
+            result[i] = n % 10;
+            n /= 10;
+        }
+        return result[0] - result[1] != result[1] - result[2];
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(bf.readLine());
+        if (num == 1000){
+            num -= 1;
+        }
+        boolean[] arr = new boolean[num];
+        int result = 0;
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = num(i +1);
+        }
+        for (int j = 0; j < arr.length; j++) {
+            if (!arr[j]){
+                result += 1;
+            }
+        }
+        System.out.println(result);
+    }
+}
