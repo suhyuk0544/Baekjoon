70c1ded3eee5d28b52b540ec93255b55ec141e17
suhyuk0544 <85779660+suhyuk0544@users.noreply.github.com>
2022-05-25 20:50:24 +0900

    update

--- src/기본수학2/problem1929.java
+++ src/기본수학2/problem1929.java
@@ -1,37 +1,33 @@
 package 기본수학2;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.StringTokenizer;
 
 public class problem1929 {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(bf.readLine());
         int min = Integer.parseInt(st.nextToken()),max = Integer.parseInt(st.nextToken());
-        boolean[] booleans = new boolean[max - min + 1];
         for (int i = min; i <= max; i++) {
-            if (i == 2) {
-                booleans[i - min] = false;
-            }else if (i % 2 == 0 || i < 2){
-                booleans[i - min] = true;
-            }else {
+            boolean booleans = false;
+            if (i % 2 == 0 || i < 2) {
+                booleans = true;
+            }else{
                 for (int j = 2; j <= Math.sqrt(max); j++) {
                     if (i != j) {
                         if (i % j == 0) {
-                            booleans[i - min] = true;
+                            booleans = true;
                             break;
                         }
                     }
                 }
+            }if (i == 2) {
+                booleans = false;
+            }if (!booleans){
+                bw.write(i+"\n");
             }
         }
-        for (int k = 0; k < booleans.length; k++) {
-            if (!booleans[k]){
-                System.out.println(k + min);
-            }
-        }
+        bw.flush();
     }
-
 }
