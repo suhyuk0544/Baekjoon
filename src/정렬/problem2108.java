55ee76adc132428a62e304f2f06632b3b1a65e57
suhyuk0544 <85779660+suhyuk0544@users.noreply.github.com>
2022-06-05 16:51:07 +0900

    update

--- src/정렬/problem2108.java
+++ src/정렬/problem2108.java
@@ -6,54 +6,39 @@ import java.util.Arrays;
 public class problem2108 {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int n = Integer.parseInt(bf.readLine());
         int[] arr = new int[n];
-        int num = 0;
+        int[] count = new int[8001];
+        int sum = 0;
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(bf.readLine());
-            num += arr[i];
+            sum += arr[i];
+        }
+        for (int i = 0; i < n; i++) {
+            count[arr[i] + 4000]++;
         }
         Arrays.sort(arr);
-        int m = mode(arr);
-        bw.write(Math.round(num / n)+"\n");
-        bw.write(arr[Math.round(n / 2)]+"\n");
-        if (m == -1){
-            if (arr.length != 1) {
-                bw.write(arr[1] + "\n");
-            }else {
-                bw.write(arr[0] + "\n");
-            }
-        }else {
-            bw.write(m + "\n");
-        }
-        bw.write(arr[n -1] - arr[0]+"\n");
-        bw.flush();
-        bw.close();
+        System.out.println((int) Math.round((double) sum / n));
+        System.out.println(arr[n / 2]);
+        System.out.println(mode(count));
+        System.out.println(arr[n-1] - arr[0]);
     }
-    static int mode(int[] arr){
-        int[][] count = new int[arr.length][2];
-        for (int i = 0; i < arr.length; i++) {
-            int num = arr[i];
-            count[i][0] = arr[i];
-            for (int u:arr) {
-                if (num == u){
-                    count[i][1]++;
+    static int mode(int[] count){
+        int max = 0;
+        int mode = 10000;
+        boolean r = false;
+        for (int j = 0; j < count.length; j++) {
+            if (count[j] != 0) {
+                if (max < count[j]) {
+                    max = count[j];
+                    mode = j - 4000;
+                    r = true;
+                } else if (max == count[j] && r) {
+                    mode = j - 4000;
+                    r = false;
                 }
             }
         }
-//        int[] num = {count[0][0],count[0][1]};
-//        for (int[] u:count) {
-//            System.out.println(Arrays.toString(u));
-//            if (max < u[1]){
-//                if (index == u[0]) {
-//                    return -1;
-//                }
-//                max = u[1];
-//                index = u[0];
-//            }
-//        }
-//        return index;
-        return 1;
+        return mode;
     }
 }
