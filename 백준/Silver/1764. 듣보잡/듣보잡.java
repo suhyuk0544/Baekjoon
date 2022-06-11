90b0e8aaac892171e889e150dd109a3d17b9ff01
suhyuk Jang <85779660+suhyuk0544@users.noreply.github.com>
2022-06-11 16:32:49 +0900

    [Silver IV] Title: 듣보잡, Time: 404 ms, Memory: 29712 KB -BaekjoonHub

---
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        int n = Integer.parseInt(st.nextToken()),m = Integer.parseInt(st.nextToken());
+        HashSet<String> hashSet = new HashSet<>();
+        List<String> str = new ArrayList<>();
+        for (int i = 0; i < n+m; i++) {
+            String s = bf.readLine();
+            if (hashSet.contains(s)){
+                str.add(s);
+            }else {
+                hashSet.add(s);
+            }
+        }
+        Collections.sort(str);
+        bw.write(str.size()+"\n");
+        for (String s:str) {
+            bw.write(s+"\n");
+        }
+        bw.flush();
+    }
+}
